fcfdcbed02467f8a1ea49fd95d37c27a4d2ca906
vincent <vincent.gong@abmgood.com>
2018-12-06 17:01:27 -0800

    info update

--- pcr_project/editing.py
+++ pcr_project/editing.py
@@ -1,4 +1,29 @@
-f1 = open('multi_comp_kit_components.sql','r+')
-f2 = open('new.txt','w+')
-content = f1.read(100)
-f2.write(content)
+import mysql.connector
+import csv
+
+def A(str):
+	return str+'-vin'
+def C(str):
+	
+
+
+
+	
+cnx=mysql.connector.connect(host='localhost', user='root', password='vincent', database='pcr_tables')
+cursor = cnx.cursor()
+
+query=("select kit_number FROM `multi_comp_kit_components` GROUP BY kit_number;")
+cursor.execute(query)
+result = cursor.fetchall()
+for catno in result:
+	print(appendcatno(catno[0]))
+
+
+
+
+
+
+# with open('editing.csv', 'a', newline='') as f:
+# 	writer = csv.writer(f)
+# 	writer.writerow([4,5,6])
+# 	writer.writerow([7,8,9])
