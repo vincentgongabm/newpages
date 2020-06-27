ffdbef5a0e07d23447830c2ddbc1e8a21d3660cb
Wenzhen Gong <kong20100902@hotmail.com>
2020-06-26 23:16:32 -0700

    update

---
+++ vector/AAV vector upload adding 5 features for AAV expression products and  other AAV vector corrections/correct vector and insert size.sql
@@ -0,0 +1,42 @@
+update abm_customsearch 
+set insert_size = 3200, vector_size = 4108
+where catno REGEXP '^3694010[0-9]{3}10$' and vector like '%pAAV-G-CMV-amp%';
+update abm_customsearch 
+set insert_size = 2200, vector_size = 5157
+where catno REGEXP '^3694010[0-9]{3}10$' and vector like '%pAAV-G-CMV-SV40-GFP-hGH-amp%';
+update abm_customsearch 
+set insert_size = 1270, vector_size = 6086
+where catno REGEXP '^3694010[0-9]{3}10$' and vector like '%pAAV-G-CMV-SV40-Luc-hGH-amp%';
+update abm_customsearch 
+set insert_size = 2560, vector_size = 4716
+where catno REGEXP '^3694010[0-9]{3}10$' and vector like '%pAAV-G-EF1a-hGH-amp%';
+update abm_customsearch 
+set insert_size = 1520, vector_size = 5828
+where catno REGEXP '^3694010[0-9]{3}10$' and vector like '%pAAV-G-EF1a-SV40-GFP-hGH-amp%';
+update abm_customsearch 
+set insert_size = 600, vector_size = 6757
+where catno REGEXP '^3694010[0-9]{3}10$' and vector like '%pAAV-G-EF1a-SV40-Luc-hGH-amp%';
+update abm_customsearch 
+set insert_size = 3300, vector_size = 4058
+where catno REGEXP '^3694010[0-9]{3}10$' and vector like '%pAAV-G-hSyn-hGH-amp%';
+update abm_customsearch 
+set insert_size = 2220, vector_size = 5136
+where catno REGEXP '^3694010[0-9]{3}10$' and vector like '%pAAV-G-hSyn-SV40-GFP-hGH-amp%';
+update abm_customsearch 
+set insert_size = 1300, vector_size = 6065
+where catno REGEXP '^3694010[0-9]{3}10$' and vector like '%pAAV-G-hSyn-SV40-Luc-hGH-amp%';
+update abm_customsearch 
+set insert_size = 3250, vector_size = 4087
+where catno REGEXP '^3694010[0-9]{3}10$' and vector like '%pAAV-G-PGK-hGH-amp%';
+update abm_customsearch 
+set insert_size = 2200, vector_size = 5149
+where catno REGEXP '^3694010[0-9]{3}10$' and vector like '%pAAV-G-PGK-SV40-GFP-hGH-amp%';
+update abm_customsearch 
+set insert_size = 1263, vector_size = 6078
+where catno REGEXP '^3694010[0-9]{3}10$' and vector like '%pAAV-G-PGK-SV40-Luc-hGH-amp%';
+update abm_customsearch 
+set insert_size = 2000, vector_size = 5340
+where catno REGEXP '^3694010[0-9]{3}10$' and vector like '%pAAV-G-CAGGS-hGH-amp%';
+update abm_customsearch 
+set insert_size = 970, vector_size = 6396
+where catno REGEXP '^3694010[0-9]{3}10$' and vector like '%pAAV-G-CAGGS-SV40-GFP-hGH-amp%';
