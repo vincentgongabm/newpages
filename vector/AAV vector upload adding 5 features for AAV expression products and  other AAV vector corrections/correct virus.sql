5897e3c057196c6222c0a1a20e2a215c10db0384
Wenzhen Gong <kong20100902@hotmail.com>
2020-06-27 23:27:04 -0700

    update

---
+++ vector/AAV vector upload adding 5 features for AAV expression products and  other AAV vector corrections/correct virus.sql
@@ -0,0 +1,3 @@
+select * from abm_customsearch
+where catno REGEXP '^36940101011[1-9]$' and vector like '%pAAV-G-CMV-amp%';
+-- where catno REGEXP '^36940101[0-9]{2}1[1-9]$' and vector like '%pAAV-G-CMV-amp%';
