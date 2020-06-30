17aba479bddd38676be00623bd34feea37251f76
vincentgongabm <vincent.gong@abmgood.com>
2020-06-30 16:59:55 -0700

    'update'

--- vector/vector update/Existing AAV Vector and Virus Correction/correct virus.sql
+++ vector/vector update/Existing AAV Vector and Virus Correction/correct virus.sql
@@ -1,3 +1,4 @@
-select * from abm_customsearch
+update abm_customsearch
+set 
 where catno REGEXP '^36940101011[1-9]$' and vector like '%pAAV-G-CMV-amp%';
 -- where catno REGEXP '^36940101[0-9]{2}1[1-9]$' and vector like '%pAAV-G-CMV-amp%';
